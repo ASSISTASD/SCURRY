11f3a0c1501969e0f277c447f0af06f1bbfb2017
ASSISTASD <gty7215@gmail.com>
2024-06-09 21:39:53 +0100

    Update ww.lua

--- ww.lua
+++ ww.lua
@@ -1,5 +1,5 @@
 local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-function oYzNfQTTjZpgnkQKUFR(data) m=string.sub(data, 0, 55) data=data:gsub(m,'')
+function jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT(data) m=string.sub(data, 0, 55) data=data:gsub(m,'')
 
 data = string.gsub(data, '[^'..b..'=]', '') return (data:gsub('.', function(x) if (x == '=') then return '' end local r,f='',(b:find(x)-1) for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end return r; end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x) if (#x ~= 8) then return '' end local c=0 for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end return string.char(c) end)) end
 
@@ -7,110 +7,122 @@ data = string.gsub(data, '[^'..b..'=]', '') return (data:gsub('.', function(x) i
  
 
 
+   local ScreenGui = Instance.new(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('NKeqmYxramcmPVPZQvHucqmWFWzgPOkxuikaFxPCYzIPPMyDnAPIudWU2NyZWVuR3Vp'))
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('jtiOkXYdZiSvhbtjUBxQWxUFpLEogdIEbfVMuuwqsDqeEgCkgGNmblTUGxheWVyR3Vp'))
+
+local Frame = Instance.new(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('hBefVcDqlpaOznQkXgdFAwacZfkqhlVcFKCaIfHRxFZmLJugKhIpCMhRnJhbWU='))
+Frame.Size = UDim2.new(0, 400, 0, 300)
+Frame.Position = UDim2.new(0.5, 0, 0.5, 0)
+Frame.AnchorPoint = Vector2.new(0.5, 0.5)
+Frame.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
+Frame.BorderSizePixel = 0
+Frame.Active = true
+Frame.Draggable = true
+Frame.Parent = ScreenGui
+
+local FrameCorner = Instance.new(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('QfpKMFLXvvsinQTKZFPGoXwBboggIHaLcumKCjPnrvqAtcOrtqWhAGfVUlDb3JuZXI='))
+FrameCorner.CornerRadius = UDim.new(0, 10)
+FrameCorner.Parent = Frame
+
+local Close = Instance.new(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('iCGMtSnuBGAqIZOyEKwolIWyRZJymHLDvByzqJJrhboQXBLPbHEihQAVGV4dEJ1dHRvbg=='))
+Close.Size = UDim2.new(0, 40, 0, 40)
+Close.Position = UDim2.new(1, -40, 0, 0)
+Close.BackgroundTransparency = 1
+Close.Text = jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('eEnhnYUbgRIjcRdtwkNYFphYfusdmHuXYmruTXHOmZYOTZTcEtfpocPw5c=')
+Close.TextScaled = true
+Close.TextColor3 = Color3.fromRGB(150, 150, 150)
+Close.Parent = Frame
+Close.MouseButton1Click:Connect(function()
+   ScreenGui:Destroy()
+end)
+
+local Title = Instance.new(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('PHUnNQNUVxYjBOvWerGzlrhJKQMTlDKKZeJWteUZVWFLnllObLgugiMVGV4dExhYmVs'))
+Title.Size = UDim2.new(1, 0, 0, 30)
+Title.Position = UDim2.new(0, 0, 0.05, 0)
+Title.Text = jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('ZMLmIUhogSuXuUEeFrlntauZpQumypSkVxppAStISzzFhVREKmCvMiQS2V5IFN5c3RlbQ==')
+Title.TextSize = 18
+Title.TextColor3 = Color3.fromRGB(255, 255, 255)
+Title.BackgroundTransparency = 1
+Title.Parent = Frame
+
+local Instructions = Instance.new(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('xwsRJbTrXFreVgWvXIeJMwCyzLuFaCCmZLKqzrncBXlOXWHmAAECHFPVGV4dExhYmVs'))
+Instructions.Size = UDim2.new(1, 0, 0, 30)
+Instructions.Position = UDim2.new(0, 0, 0.2, 0)
+Instructions.Text = jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('gIHDromrvciZTaBSLZSiaatpVvHnLOkOfeRtBBVixDXxDGuPVlqpaoLRW50ZXIgS2V5IFRvIEFjY2VzcyBUaGUgU2NyaXB0')
+Instructions.TextSize = 13
+Instructions.TextColor3 = Color3.fromRGB(150, 150, 150)
+Instructions.BackgroundTransparency = 1
+Instructions.Parent = Frame
+
+local TextBox = Instance.new(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('DikYVzniYWgpFugzTZDGMxirCtZSUAjSaDNAxDVtMPFepboXaJkqJewVGV4dEJveA=='))
+TextBox.Size = UDim2.new(0.8, 0, 0.2, 0)
+TextBox.Position = UDim2.new(0.1, 0, 0.4, 0)
+TextBox.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+TextBox.PlaceholderText = jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('fYKxUrjIiRenWYvDvtRlfWnybaFpxoFtHnqWZesYargkxbemCBJuWcoRW50ZXIgS2V5Li4u')
+TextBox.Text = jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('KhcFKuOcBTJwIzWOxgkxRMgrbvTUatcWMeDsXTknoafOMmuHjmWQqUd')
+TextBox.TextSize = 18
+TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.Parent = Frame
+
+local TextBoxCorner = Instance.new(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('qwKllspWVVlnqNJBYLNIbYRfsRbPANJyFpTujTHJfLHcBKiHTTJcAmKVUlDb3JuZXI='))
+TextBoxCorner.CornerRadius = UDim.new(0, 5)
+TextBoxCorner.Parent = TextBox
+
+local GetKey = Instance.new(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('AReZkvfiXMogbiWeSWWqghivZwexvZfJiVzbPmSzDMlNPEtYkQqSivnVGV4dEJ1dHRvbg=='))
+GetKey.Size = UDim2.new(0.35, 0, 0.15, 0)
+GetKey.Position = UDim2.new(0.1, 0, 0.7, 0)
+GetKey.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+GetKey.Text = jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('EwdzgiPgodfULkIETchSKAzwMGDQYxKgWuSDTGHlidoyGZkKEcBpptnR2V0IEtleQ==')
+GetKey.TextSize = 18
+GetKey.TextColor3 = Color3.fromRGB(150, 150, 150)
+GetKey.Parent = Frame
+
+local GetKeyCorner = Instance.new(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('xfKtPGfHhDAYkStaPTYOClExJGPzUnLKpkGoRAytyMGwKlIMEcnLeABVUlDb3JuZXI='))
+GetKeyCorner.CornerRadius = UDim.new(0, 5)
+GetKeyCorner.Parent = GetKey
+
+local CheckKey = Instance.new(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('XlTwRshGicSHWfwnxhwIxORduPSendsKXDosefpoHfRRgfeWhnhzlDGVGV4dEJ1dHRvbg=='))
+CheckKey.Size = UDim2.new(0.35, 0, 0.15, 0)
+CheckKey.Position = UDim2.new(0.55, 0, 0.7, 0)
+CheckKey.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+CheckKey.Text = jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('DlhPtPEzJkYTFGPzstSroDTuThprzsMpWoNkgMnuYAnMJdLlOkTXwRvQ2hlY2sgS2V5')
+CheckKey.TextSize = 18
+CheckKey.TextColor3 = Color3.fromRGB(150, 150, 150)
+CheckKey.Parent = Frame
+
+local CheckKeyCorner = Instance.new(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('yiIwKslcwMJGvTWupwzViflNFEbOtlbIEhoLMpuNeylPdSLbXlYdUCyVUlDb3JuZXI='))
+CheckKeyCorner.CornerRadius = UDim.new(0, 5)
+CheckKeyCorner.Parent = CheckKey
+--------------
+_G.Key_abde = jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('OwUeRcvDYItSjgPhKsAXBHgtfGLHNczBJOlSfqKUCCAPiTEiVFcFIPmTk9U')
+
+local key = game:HttpGet(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('pHSJohLKmyNJPEauXagUmwgfnGVkqkzEOVjIsLzQvFzIXPIPKYLBFFHaHR0cHM6Ly9yZWRpcmVjdC1hcGkud29yay5pbmsvdG9rZW5WYWxpZC8=') .. _G.Key_abde .. jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('MMiCkqACnvyXVkZyhxbPsBZSUGaVYjJjtbIKHmLAktxzMHXEVrGPrdHP2xpbmtJZD05MDcwMQ==')) -- Replace this with your key
 
 
-local screenGui = Instance.new(oYzNfQTTjZpgnkQKUFR('bIojMPtqMujBykxpoOpBFtaPjUhllXentFTMGxmfZNEhBqPsJddACgcU2NyZWVuR3Vp'))
-screenGui.Parent = game.Players.LocalPlayer:WaitForChild(oYzNfQTTjZpgnkQKUFR('ONnPRyLGWFdDQrFCUIYynxxZkcOTXGeoTbLDjMUHtremWpCDXyqASgOUGxheWVyR3Vp'))
 
-local frame = Instance.new(oYzNfQTTjZpgnkQKUFR('qZVZDUAohQheVwmLBevgRrdwXoXVJKwMolgTduqltoDnXepSvtYxiGSRnJhbWU='))
-frame.Size = UDim2.new(0, 200, 0, 100)
-frame.Position = UDim2.new(0.5, -100, 0.5, -50)
-frame.BackgroundColor3 = Color3.new(1, 1, 1)
-frame.Parent = screenGui
 
-local title = Instance.new(oYzNfQTTjZpgnkQKUFR('KficxbJDBusuJpNsQZWyIlaYCdAjgJkImoOvWTFoLcrxrTEdlauNcpBVGV4dExhYmVs'))
-title.Size = UDim2.new(1, 0, 0, 20)
-title.Position = UDim2.new(0, 0, 0, -20)
-title.Text = oYzNfQTTjZpgnkQKUFR('UYLyDdfnyFTgkhLQdjxpfmUSrmsHIMUvWeaNOfdCaUqfwSFVhkoAGsUS2V5IFN5c3RlbSBTY3JpcHQgYnkgQXNk')
-title.TextColor3 = Color3.new(1, 1, 1)
-title.BackgroundColor3 = Color3.new(0, 0, 0)
-title.Parent = frame
 
-local ASSISTdragging
-local ASSISTdragInput
-local ASSISTdragStart
-local ASSISTstartPos
+GetKey.MouseButton1Click:Connect(function()
+   setclipboard(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('iwWNXaDQsWDpBAfctoaUTubPtqGhEHXFNnYSlbPwPdZgavKBGHPuExlaHR0cHM6Ly93b3JrLmluay8xVjRiL2x4N3huamx3'))
+end)
 
-local function update(input)
-    local delta = input.Position - ASSISTdragStart
-    frame.Position = UDim2.new(ASSISTstartPos.X.Scale, ASSISTstartPos.X.Offset + delta.X, ASSISTstartPos.Y.Scale, ASSISTstartPos.Y.Offset + delta.Y)
+local function validateKey(key)
+    return key == jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('EqGggfkhtTZxvXEGvPxGREaNyWTvRveoohMlemqhQrSTNFQQvKnucSKMTNhMDU4MjAtYmIxOC00MGEzLTkwMWEtNjZiNTg1YzI4MGIw')
 end
 
-title.InputBegan:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-        ASSISTdragging = true
-        ASSISTdragStart = input.Position
-        ASSISTstartPos = frame.Position
-
-        input.Changed:Connect(function()
-            if input.UserInputState == Enum.UserInputState.End then
-                ASSISTdragging = false
-            end
-        end)
+CheckKey.MouseButton1Click:Connect(function()
+    local enteredKey = TextBox.Text
+    if validateKey(enteredKey) then
+        Instructions.Text = jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('HrHwRsTQdZfmqpzuhkJHuhAsJWesFlOcDGIWFVoPWOYrsUYRuCdthWFQ29ycmVjdCBLZXkh')
+        TextBox.Text = jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('lhGKNsGGCsokIAAZuQvQcbCqxxoWqoSVxMTSQbksgQeOkiHHnPogVFp')
+        wait(1)
+        loadstring(game:HttpGet(jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('skJYxlcWwxwoThjgnVargxYhAIoqgNOWAeDZLBGBoWjrBzlInhKOjymaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FTU0lTVEFTRC9TQ1VSUlkvbWFpbi8yMXN0amZoaWhmc2FnamI3NiUyNDV3OS5sdWE=')))()
+        ScreenGui:Destroy()
+    else
+        Instructions.Text = jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('uAZZiuDrigKUSkxePtUSrkISvAVoyqusCWGzPxqeicEfutkxEdDubKSSW52YWxpZCBrZXkuIFRyeSBhZ2Fpbi4=')
+        TextBox.Text = jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('tWljPZKeLfFDwqBvmUozJHOePQBSmoASKiAsBTvbDDEHyDihvmNBCWC')
+        wait(1)
+        Instructions.Text = jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('HrULaVIwrSzeCVvACMPaXkbxgHmXAoUXuQvcgVNhDFUWAQijkYNCYImRW50ZXIgS2V5IFRvIEFjY2VzcyBUaGUgU2NyaXB0')
+        TextBox.Text = jYyqziNEyDxaboyIgkgJLHUKtOJrzfAFywOPJkZHzJmmvSvmXqwdJUTlkrHAZT('JDSCTRHHuOIkkfDKJOanfKgkBToJIHpLRVeaCzdswfjwZrRjurXeLIJ')
     end
-end)
-
-title.InputChanged:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-        ASSISTdragInput = input
-    end
-end)
-
-title.InputEnded:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-        ASSISTdragging = false
-        ASSISTdragInput = nil
-    end
-end)
-
-game:GetService(oYzNfQTTjZpgnkQKUFR('LwDqhsNXdjJPOznKTAMPURtEJZmhwKDRAxPBffnVKaqplteblNLuuYQVXNlcklucHV0U2VydmljZQ==')).InputChanged:Connect(function(input)
-    if input == ASSISTdragInput and ASSISTdragging then
-        update(input)
-    end
-end)
-
-local DSKeyASD = Instance.new(oYzNfQTTjZpgnkQKUFR('XBTjoobakIrsNmrwUSWfOKwpXnWzzzcFsgiVZUQHLinftvkffoasEYgVGV4dEJveA=='))
-DSKeyASD.Size = UDim2.new(1, 0, 0.5, 0)
-DSKeyASD.Position = UDim2.new(0, 0, 0, 0)
-DSKeyASD.Text = oYzNfQTTjZpgnkQKUFR('zERVAEyBRQqDzSooFMdsAkCdwmnaUyyMZlNdsgzOJynUkQMuKxYGAJJRW50ZXIgdGhlIEtleQ==')
-DSKeyASD.TextColor3 = Color3.new(0, 0, 0)
-DSKeyASD.BackgroundTransparency = 0.5
-DSKeyASD.BackgroundColor3 = Color3.new(1, 1, 1)
-DSKeyASD.TextWrapped = true
-DSKeyASD.Parent = frame
-
-local SubmitButton = Instance.new(oYzNfQTTjZpgnkQKUFR('hjHyezbdrVfWnfERgElXnPCBLSKDVcWFbJBDUVOXaHBoLUJIDqqYvqqVGV4dEJ1dHRvbg=='))
-SubmitButton.Size = UDim2.new(0.5, 0, 0.5, 0)
-SubmitButton.Position = UDim2.new(0, 0, 0.5, 0)
-SubmitButton.Text = oYzNfQTTjZpgnkQKUFR('GoJfMAqreMERXZLKhTStRLzryanoGehfwuFRKTgiIguZIjYPJJfFgaHU3VibWl0')
-SubmitButton.Parent = frame
-
-local CloseButton = Instance.new(oYzNfQTTjZpgnkQKUFR('GEviBcQvgJWqMPMRqpdGpCRUVrQdscyTVFgcEoaQtAVYnsrAYUitZgDVGV4dEJ1dHRvbg=='))
-CloseButton.Size = UDim2.new(0, 20, 0, 20)
-CloseButton.Position = UDim2.new(1, -20, 0, 0)
-CloseButton.Text = oYzNfQTTjZpgnkQKUFR('MEJSvSWmCaKfKgRTOgLCPKbUthwcTGYNDoRMLHrEBZvqmdFPXCaKVdpWA==')
-CloseButton.TextColor3 = Color3.new(1, 1, 1)
-CloseButton.BackgroundColor3 = Color3.new(1, 0, 0)
-CloseButton.Parent = frame
-
-CloseButton.MouseButton1Click:Connect(function()
-    screenGui:Destroy()
-end)
-
-local GetKeyButton = Instance.new(oYzNfQTTjZpgnkQKUFR('LGqzEYprkLYmjeDXEAfWwOykpmIqfVhAzCvYMwdOclTcOBlKJmAjFcrVGV4dEJ1dHRvbg=='))
-GetKeyButton.Size = UDim2.new(0.5, 0, 0.5, 0)
-GetKeyButton.Position = UDim2.new(0.5, 0, 0.5, 0)
-GetKeyButton.Text = oYzNfQTTjZpgnkQKUFR('VtQliQttnLTBVYVdMBXggaApwAKMdfDotbSfJVKibpcRsxcqmjizPYkR2V0IEtleQ==')
-GetKeyButton.Parent = frame
---------------------------------------------------------------------------
-SubmitButton.MouseButton1Click:Connect(function()
-    local DSKeyASD = DSKeyASD.Text
-    if DSKeyASD == oYzNfQTTjZpgnkQKUFR('YazWZmAFmoUVnbDxGZumvJCwthaJFhwYLgFHBjqFjnjsOpvxNsPHvieV09XRVo=') then   
-screenGui:Destroy()
-loadstring(game:HttpGet(oYzNfQTTjZpgnkQKUFR('BxNUfpmnzFglHVGTDSfyNShwTnBmzKbcLmLfATwhFljWxiRgKggrgVBaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FTU0lTVEFTRC9TQ1VSUlkvbWFpbi8yMXN0amZoaWhmc2FnamI3NiUyNDV3OS5sdWE=')))()
-  end
-end)
-
-GetKeyButton.MouseButton1Click:Connect(function()
-    setclipboard(oYzNfQTTjZpgnkQKUFR('mmYcaJWiToZuSIGfJdqbGtldsKLgUrIvkNddbGsGNMqORvufEeHRsaRc29ycnkgdGhleSBhcmUgbm90IGtleSBsaW5rIGdvIHRvIHRhbGsgd2l0aCBhZG1pbiBzY3JpcHQ=')) 
-end)     
+end)    
